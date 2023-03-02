edd0fb3a1e860e5fc0bcfec835338a32b3add382
orjiugo <orjiugo.victor@gmail.com>
2023-03-02 12:36:37 -0800

    Here

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
- */
+* rev_string - reverse array
+* @n: integer params
+* Return: 0
+*/
 
 void rev_string(char *n)
 {
@@ -27,13 +27,13 @@ void rev_string(char *n)
 }
 
 /**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
+* infinite_add - add 2 numbers together
+* @n1: text representation of 1st number to add
+* @n2: text representation of 2nd number to add
+* @r: pointer to buffer
+* @size_r: buffer size
+* Return: pointer to calling function
+*/
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
