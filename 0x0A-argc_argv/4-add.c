c6486970a3599ed2742424d09c67f1645ac7706f
orjiugo <orjiugo.victor@gmail.com>
2023-03-10 16:07:34 -0800

    here

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,83 +2,55 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
 /**
- * check_num - check - string there are digit
- * @str: array str
- * main - Print the name of the program
- * Return: Always 0 (Success)
+ * check_num - check if string contains only digits
+ * @str: array of characters
+ *
+ * Return: 1 if the string contains only digits, 0 otherwise
  */
-	int check_num(char *str)
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
 	{
-		/*Declaring variables*/
-		unsigned int count;
-
-
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-
-
+		if (!isdigit(str[count]))
 		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
-			{
-				return (0);
-			}
-
-
-			count++;
+			return (0);
 		}
-		return (1);
+		count++;
 	}
+	return (1);
+}
 
+/**
+ * main - adds positive numbers
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int count, str_to_int, sum = 0;
 
-	/**
-	 * check_num - check - string there are digit
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-
-
-	int main(int argc, char *argv[])
-
-
+	count = 1;
+	while (count < argc)
 	{
-
-
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
-
-
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
+		if (check_num(argv[count]))
 		{
-			if (check_num(argv[count]))
-
-
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-
-
-			/*Condition if one of the number contains symbols that are not digits*/
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-
-			count++;
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
 		}
-
-
-		printf("%d\n", sum); /*print sum*/
-
-
-		return (0);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
+	printf("%d\n", sum);
+	return (0);
+}
